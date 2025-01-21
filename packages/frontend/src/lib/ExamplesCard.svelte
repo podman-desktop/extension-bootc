c5d891d60aa19957214f93188d0dd4ef37b77a10
Tim deBoer <git@tdeboer.ca>
2025-01-21 11:13:15 -0500

    chore: update Card to svelte 5
    Just updates the child rendering to svelte 5 to remove the build warning. Since
    this is the only slot I just removed the name for simplicity.
    
    Fixes #1206.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/frontend/src/lib/ExamplesCard.svelte
+++ packages/frontend/src/lib/ExamplesCard.svelte
@@ -64,7 +64,7 @@ $effect(() => {
 </script>
 
 <Card title={category.name}>
-  <div slot="content" class="w-full">
+  <div class="w-full">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
       {#each examples as example}
         <ExampleCard example={example} />
