c95b6088ae7447caa41387d42dc39ad225380af2
Tim deBoer <git@tdeboer.ca>
2025-02-25 13:45:49 -0500

    chore: update dashboard to match new design
    The bootc extension dashboard should be updated to match the UX of the
    Kubernetes dashboard, but there is no reusable component yet [1]. We want to
    do a release soon, so there isn't time to wait for a component.
    
    In order to avoid completely copy/paste/editting the current Kubernetes dashboard,
    I've added several temporary components to the /upstream folder:
    - An expandable component that I've just opened a PR for [2], minus animation to
      reduce temporary dependencies.
    - Resource and guide cards that are almost 100% copy/pasted from Podman Desktop.
    - A DashboardPage that does a rough componentization of the Kubernetes dashboard.
      The final one might not look exactly like this, but it avoids most future-work
      within bootc.
    
    And finally - an updated Bootc Dashboard that uses the component. Notes:
    - The Bootc Images card has no link (since we plan to add a page next milestone)
    - Disk image card links to the corresponding page.
    
    [1] Dashboard component: https://github.com/podman-desktop/podman-desktop/issues/11363
    [2] Expandable: https://github.com/podman-desktop/podman-desktop/pull/11364
    
    Fixes #976.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/frontend/src/lib/upstream/DashboardGuideCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { Button } from '@podman-desktop/ui-svelte';
+import { bootcClient } from '/@/api/client';
+
+interface Props {
+  title: string;
+  link: string;
+  image: string;
+}
+
+let { title, link, image }: Props = $props();
+
+async function openLink(): Promise<void> {
+  await bootcClient.openLink(link);
+}
+</script>
+
+<div class="flex flex-col flex-grow gap-4 p-4 bg-[var(--pd-content-card-carousel-card-bg)] hover:bg-[var(--pd-content-card-carousel-card-hover-bg)] rounded-md items-center">
+  <img src={image} class="max-w-[100%] h-24 object-contain rounded-md" alt={`${title} image`} />
+  <div class="text-[var(--pd-content-card-carousel-card-header-text)] text-center font-semibold">{title}</div>
+  <Button on:click={openLink}>Read more</Button>
+</div>
