ff2289dcc5f920c5db7706afd61cb346d5664871
Tim deBoer <git@tdeboer.ca>
2025-02-25 13:45:49 -0500

    chore: update dashboard to match new design
    The bootc extension dashboard should be updated to match the UX of the
    Kubernetes dashboard, but there is no reusable component yet [1]. We want to
    do a release soon, so there isn't time to wait for a component.
    
    In order to avoid completely copy/paste/editting the current Kubernetes dashboard,
    I've added several temporary components to the /upstream folder:
    - An expandable component that I've just opened a PR for [2], minus animation to
      reduce temporary dependencies.
    - Resource and guide cards that are almost 100% copy/pasted from Podman Desktop.
    - A DashboardPage that does a rough componentization of the Kubernetes dashboard.
      The final one might not look exactly like this, but it avoids most future-work
      within bootc.
    
    And finally - an updated Bootc Dashboard that uses the component. Notes:
    - The Bootc Images card has no link (since we plan to add a page next milestone)
    - Disk image card links to the corresponding page.
    
    [1] Dashboard component: https://github.com/podman-desktop/podman-desktop/issues/11363
    [2] Expandable: https://github.com/podman-desktop/podman-desktop/pull/11364
    
    Fixes #976.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/frontend/src/lib/dashboard/Dashboard.svelte
+++ packages/frontend/src/lib/dashboard/Dashboard.svelte
@@ -6,18 +6,31 @@ import { onMount, tick } from 'svelte';
 import { bootcClient, rpcBrowser } from '../../api/client';
 import { Messages } from '/@shared/src/messages/Messages';
 import { router } from 'tinro';
-import { Button, NavPage } from '@podman-desktop/ui-svelte';
+import { Button } from '@podman-desktop/ui-svelte';
 import type { ImageInfo } from '@podman-desktop/api';
+import DashboardPage from '../upstream/DashboardPage.svelte';
+import DashboardResourceCard from '../upstream/DashboardResourceCard.svelte';
+import DiskImageIcon from '../DiskImageIcon.svelte';
+import Expandable from '../upstream/Expandable.svelte';
+import DashboardGuideCard from '../upstream/DashboardGuideCard.svelte';
+import { historyInfo } from '../../stores/historyInfo';
+import osbuildImage from './osbuild.png';
+import rhdImage from './rhd.png';
+import fedoraImage from './fedora.png';
 
 let pullInProgress = $state(false);
 let displayDisclaimer = $state(false);
 let bootcAvailableImages: ImageInfo[] = $state([]);
+
+let bootcImageCount = $derived(bootcAvailableImages.length);
+let diskImageCount = $derived($historyInfo.length);
 
 const exampleImage = 'quay.io/bootc-extension/httpd:latest';
 const bootcImageBuilderSite = 'https://github.com/osbuild/bootc-image-builder';
 const bootcSite = 'https://containers.github.io/bootc/';
 const fedoraBaseImages = 'https://docs.fedoraproject.org/en-US/bootc/base-images/';
 const extensionSite = 'https://github.com/containers/podman-desktop-extension-bootc';
+const rhdImageMode = './RHD.png';
 
 async function gotoBuild(): Promise<void> {
   // Split the image name to get the image name and tag
@@ -68,55 +81,75 @@ onMount(async () => {
 let imageExists = $derived(bootcAvailableImages?.some(image => image.RepoTags?.includes(exampleImage)));
 </script>
 
-<NavPage searchEnabled={false} title="Dashboard">
-  <div class="flex min-w-full h-full" slot="content">
-    <div class="flex w-full h-full justify-center items-center">
-      <div class="flex flex-col h-full items-center text-center space-y-3">
-        <!-- Bootable Container Icon -->
-        <div class="py-2">
-          <BootcSelkie size="120" />
-        </div>
 
-        <h1 class="text-xl pb-4 text-[var(--pd-content-header)]">Welcome to Bootable Containers</h1>
-
-        <p class="pb-4 max-w-xl text-[var(--pd-card-header-text)]">
+<DashboardPage>
+  <!-- eslint-disable-next-line sonarjs/no-unused-vars -->
+  {#snippet pageTitle()}
+    Welcome to Bootable Containers
+  {/snippet}
+  <!-- eslint-disable-next-line sonarjs/no-unused-vars -->
+  {#snippet header()}
+    <div class="flex flex-row">
+      <div class="flex flex-col gap-4">
+        <div>
           Bootable Containers builds an entire bootable OS from your container image. Utilizing the technology of a
           <Link externalRef={fedoraBaseImages}>compatible image</Link>, <Link externalRef={bootcImageBuilderSite}
             >bootc-image-builder</Link
-          >, and <Link externalRef={bootcSite}>bootc</Link>, your container image is transformed into a bootable disk
-          image.
-        </p>
-
-        <p class="pb-1 max-w-xl text-[var(--pd-card-header-text)]">
-          Create your first disk image by {imageExists ? 'building' : 'pulling'} the <Link
-            externalRef={`https://${exampleImage}`}>example container image</Link
-          >:
-        </p>
-
-        <!-- Build / pull buttons -->
-        {#if imageExists}
-          <Button on:click={gotoBuild} icon={faCube} aria-label="Build image" title="Build"
-            >Build {exampleImage}</Button>
-        {:else}
-          <Button
-            on:click={pullExampleImage}
-            icon={faArrowCircleDown}
-            inProgress={pullInProgress}
-            aria-label="Pull image"
-            title="Pull image">Pull {exampleImage}</Button>
-        {/if}
-        {#if displayDisclaimer}
-          <p class="text-[var(--pd-status-waiting)] text-sm">
-            The file size of the image is over 1.5GB and may take a while to download.
-          </p>
-        {/if}
-
-        <p class="pt-8 max-w-xl text-[var(--pd-card-header-text)]">
+          >, and <Link externalRef={bootcSite}>bootc</Link>, your container image is transformed into a bootable disk image.
+        </div>
+        <div>
           Want to learn more including building your own Containerfile? Check out the <Link externalRef={extensionSite}
-            >extension documentation</Link
-          >.
-        </p>
+            >extension documentation</Link>.
+        </div>
+      </div>
+      <div class="w-40 ml-8 mb-2">
+        <BootcSelkie size="90" />
       </div>
     </div>
+  {/snippet}
+
+  <div class="text-xl pt-2">Metrics</div>
+  <div class="grid grid-cols-4 gap-4">
+    <DashboardResourceCard type="Bootc Images" Icon={DiskImageIcon} count={bootcImageCount} link="/" />
+    <DashboardResourceCard type="Disk Images" Icon={DiskImageIcon} count={diskImageCount} link="/disk-images/" />
   </div>
-</NavPage>
+
+  <div class="flex flex-1 flex-col pt-2">
+    <Expandable>
+      <!-- eslint-disable-next-line sonarjs/no-unused-vars -->
+      {#snippet title()}<div class="text-xl">Explore articles and blog posts</div>{/snippet}
+      <div class="grid grid-cols-3 gap-4">
+        <DashboardGuideCard title='Image Builder User Guide' image={osbuildImage} link='https://osbuild.org/docs/user-guide/introduction/'/>
+        <DashboardGuideCard title='Introducing image mode for RHEL and bootable containers' image={rhdImage} link='https://developers.redhat.com/articles/2024/05/07/image-mode-rhel-bootable-containers'/>
+        <DashboardGuideCard title='Getting Started with Bootable Containers' image={fedoraImage} link='https://docs.fedoraproject.org/en-US/bootc/getting-started/'/>
+      </div>
+    </Expandable>
+  </div>
+
+
+  <div class="flex flex-col h-full items-center text-center space-y-3">
+    <p class="pb-1 max-w-xl text-[var(--pd-card-header-text)]">
+      Create your first disk image by {imageExists ? 'building' : 'pulling'} the <Link
+        externalRef={`https://${exampleImage}`}>example container image</Link
+      >:
+    </p>
+
+    <!-- Build / pull buttons -->
+    {#if imageExists}
+      <Button on:click={gotoBuild} icon={faCube} aria-label="Build image" title="Build"
+        >Build {exampleImage}</Button>
+    {:else}
+      <Button
+        on:click={pullExampleImage}
+        icon={faArrowCircleDown}
+        inProgress={pullInProgress}
+        aria-label="Pull image"
+        title="Pull image">Pull {exampleImage}</Button>
+    {/if}
+    {#if displayDisclaimer}
+      <p class="text-[var(--pd-status-waiting)] text-sm">
+        The file size of the image is over 1.5GB and may take a while to download.
+      </p>
+    {/if}
+  </div>
+</DashboardPage>
