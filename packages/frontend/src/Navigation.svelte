642307f881ebe27aafde6cb6d6edf9790b262b3b
Charlie Drage <charlie@charliedrage.com>
2026-02-11 11:15:58 -0500

    feat(ui): add icons to navigation bar
    ### What does this PR do?
    
    Some leftover code I had from a while ago, this adds the icons to the
    navbar similar to the podman desktop settings style.
    
    We also upgrade to the newest ui svelte in order to incorporate the
    "spacing changes" added for preferences.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/podman-desktop/extension-bootc/issues/2298
    
    ### How to test this PR?
    
    Open up bootc and see the new icons on the navbar :)
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/frontend/src/Navigation.svelte
+++ packages/frontend/src/Navigation.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 import type { TinroRouteMeta } from 'tinro';
 import { SettingsNavItem } from '@podman-desktop/ui-svelte';
+import { faTableCellsLarge, faCompactDisc, faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import BootcImageIcon from './lib/BootcImageIcon.svelte';
 
 interface Props {
   meta: TinroRouteMeta;
@@ -9,17 +11,19 @@ let { meta }: Props = $props();
 </script>
 
 <nav
-  class="z-1 w-leftsidebar min-w-leftsidebar shadow-sm flex-col justify-between flex bg-[var(--pd-secondary-nav-bg)] border-[var(--pd-global-nav-bg-border)] border-r-[1px]"
+  class="z-1 w-leftsidebar min-w-leftsidebar flex-col justify-between flex transition-all duration-500 ease-in-out bg-[var(--pd-secondary-nav-bg)] border-[var(--pd-global-nav-bg-border)] border-r-[1px]"
   aria-label="Navigation">
   <div class="flex items-center">
-    <a href="/" title="Navigate to dashboard" class="pt-4 pl-3 px-5 mb-10 flex items-center ml-[4px]">
-      <p class="text-xl font-semibold text-[color:var(--pd-secondary-nav-header-text)]">Bootable Containers</p>
+    <a href="/" title="Navigate to dashboard" class="pt-4 px-3 mb-5 flex items-center gap-3">
+      <BootcImageIcon size="28px" solid />
+      <p class="text-xl font-semibold text-[color:var(--pd-secondary-nav-header-text)] border-l-[4px] border-transparent">Bootable Containers</p>
     </a>
   </div>
-  <div class="h-full overflow-hidden hover:overflow-y-auto" style="margin-bottom:auto">
-    <SettingsNavItem title="Dashboard" selected={meta.url === '/'} href="/" />
-    <SettingsNavItem title="Images" selected={meta.url.startsWith('/images')} href="/images" />
-    <SettingsNavItem title="Disk Images" selected={meta.url.startsWith('/disk-image')} href="/disk-images" />
-    <SettingsNavItem title="Examples" selected={meta.url.startsWith('/examples')} href="/examples" />
+  <div class="h-full overflow-y-auto" style="margin-bottom:auto">
+    <SettingsNavItem title="Dashboard" icon={faTableCellsLarge} selected={meta.url === '/'} href="/" />
+    <SettingsNavItem title="Images" icon={BootcImageIcon} selected={meta.url.startsWith('/images')} href="/images" />
+    <SettingsNavItem title="Disk Images" icon={faCompactDisc} selected={meta.url.startsWith('/disk-image')} href="/disk-images" />
+    <SettingsNavItem title="Examples" icon={faBookOpen} selected={meta.url.startsWith('/examples')} href="/examples" />
   </div>
 </nav>
+
